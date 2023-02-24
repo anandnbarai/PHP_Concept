3f62daf80eb916dca8a85b5df2e42017e9044137
Anand Barai <121215880+anandnbarai@users.noreply.github.com>
2023-02-24 08:12:14 +0530

    Add files via upload

--- foreach_loop.php
+++ foreach_loop.php
@@ -9,10 +9,13 @@ foreach ($colors as $color) {
   echo "<br>";
 }
 
+// to print key value we write $name => $age in foreach loop same as in array function.
+
 $ages = array("Peter"=>32, "John"=>28, "Mary"=>45);
 
+echo "<ul>";
 foreach ($ages as $name => $age) {
-  echo "$name is $age years old.<br>";
+  echo "<li> $name : $age </li>";
 }
-
+echo "</ul>";
 ?>
---
+++ super_global_variable.php
@@ -0,0 +1,9 @@
+<?php
+
+// a superglobal variable is a special type of variable that is available in all scopes throughout your script. -->
+// These variables are automatically created by PHP and contain useful information about the server, 
+// the request, and other aspects of the current runtime environment.
+
+
+
+?>
