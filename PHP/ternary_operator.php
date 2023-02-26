08de305733116230598d03e44cb257f2b391e8ac
Anand Barai <121215880+anandnbarai@users.noreply.github.com>
2023-02-26 17:43:35 +0530

    Add files via upload

---
+++ PHP/ternary_operator.php
@@ -0,0 +1,13 @@
+<?php
+
+//! the ternary operator is basically an if, then, else in a single line.
+//? Syntax 
+//* $var = condition ? Result is condition is true : Result if condition is False
+
+$age = 19;
+
+$xyz = ($age > 18) ? "<b>You are eligible for Voting.</b>" : "<b>You are not eligible for Voting.</b>";
+
+echo $xyz;
+
+?>
