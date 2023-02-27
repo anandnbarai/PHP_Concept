4698101866d170744d67a1676eb77f5efa4508a4
Anand Barai <121215880+anandnbarai@users.noreply.github.com>
2023-02-27 11:57:42 +0530

    Add files via upload

---
+++ PHP/data_types.php
@@ -0,0 +1,16 @@
+<?php
+
+// Variables can store data of different types, and different data types can do different things.
+
+// Data type can be categorized in 3 types:
+
+//? 1. Scalar Type (Predefined)
+    // holds only single value. there are 4 scalar type variable in PHP : Boolean, Integer, Float, String
+
+//? 2. Compound Type (User Defined)
+    // there are 2 compound type variable in PHP : Array, Object
+    
+//? 3. Special Type
+    // there are 2 special type variable in PHP : Resource, NULL
+
+?>
